64ec3d0680b8872becab14b659daee82836e9982
nurzhansmile <152367784+nurzhansmile@users.noreply.github.com>
2024-01-12 05:51:22 +0600

    homework 2

--- project_JS2/js/home_works.js
+++ project_JS2/js/home_works.js
@@ -16,17 +16,68 @@ gmailButton.addEventListener('click', () => {
     }
 })
 
-// HW 1, Задачка 2
 
-const parentBlock = document.querySelector('.parent_block');
+
+// HW 1, part 2
+
+// const parentBlock = document.querySelector('.parent_block');
 const childBlock = document.querySelector('.child_block');
-let smallBlock = 0;
-const increment = () => {
-    smallBlock++
-    childBlock.style.left= `${smallBlock}px`
-    console.log(smallBlock)
-    if(smallBlock < 450){
-        requestAnimationFrame(increment);
+let positionX = 0;
+let positionY = 0;
+const maxWidthParentBlock = 449;
+const smallBlock = () => {
+    if(positionX <= maxWidthParentBlock && positionY === 0){
+    positionX++
+    childBlock.style.left = `${positionX}px`
+        requestAnimationFrame(smallBlock);
+    }else if(positionX >= maxWidthParentBlock && positionY <= maxWidthParentBlock){
+        positionY++
+        childBlock.style.top = `${positionY}px`
+        requestAnimationFrame(smallBlock);
+// HW 2, part 1
+    }else if( positionX > 0 && positionY > 0) {
+        positionX--
+        childBlock.style.left = `${positionX}px`
+        requestAnimationFrame(smallBlock);
+    }
+    else  {
+        positionY--
+        childBlock.style.top = `${positionY}px`
+        requestAnimationFrame(smallBlock);
     }
 }
-requestAnimationFrame(increment);
+requestAnimationFrame(smallBlock);
+
+
+
+// HW 2, part 2
+
+const time = document.querySelector('#seconds')
+const buttonStart = document.querySelector('#start');
+const buttonStop = document.querySelector('#stop');
+const buttonReset = document.querySelector('#reset');
+
+let seconds = 0;
+let interval = 0;
+buttonStart.addEventListener('click', () => {
+    clearInterval(interval)
+   interval = setInterval( () => {
+     seconds++
+     time.innerHTML = seconds
+     console.log(seconds)
+ },1000)
+})
+
+buttonStop.addEventListener('click', () => {
+    clearInterval(interval)
+    time.innerHTML = seconds
+    console.log("stop")
+})
+
+buttonReset.addEventListener('click', ()=> {
+    clearInterval(interval)
+    seconds = 0
+    time.innerHTML = '' + 0
+    console.log(seconds)
+})
+
