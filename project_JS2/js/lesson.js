0b5ae2238fb5f5572cd9af4b07016abb942fe5df
nurzhansmile <152367784+nurzhansmile@users.noreply.github.com>
2024-01-17 12:17:55 +0600

    Update lesson.js

--- project_JS2/js/lesson.js
+++ project_JS2/js/lesson.js
@@ -12,3 +12,54 @@ phoneButton.addEventListener('click', () => {
         phoneResult.style.color = 'red';
     }
 })
+
+// tab slider
+
+const tabContents = document.querySelectorAll('.tab_content_block')
+const tabItems = document.querySelectorAll('.tab_content_item')
+const tabsParent = document.querySelector('.tab_content_items')
+const hideTabContent =() => {
+    tabContents.forEach((tabContent) => {
+        tabContent.style.display = 'none'
+    })
+    tabItems.forEach((tabItem) => {
+        tabItem.classList.remove('tab_content_item_active')
+    })
+}
+
+const showTabContent = (index = 0) =>{
+    tabContents[index].style.display = 'block'
+    tabItems[index].classList.add('tab_content_item_active')
+}
+
+hideTabContent()
+showTabContent()
+
+
+const autoSlider = (i = 0) => {
+    setInterval(() => {
+        i++
+        if (i > tabsParent.length - 1) {
+            i = 0
+        }
+        hideTabContent()
+        showTabContent(i)
+    }, 4000)
+}
+
+tabsParent.onclick = (event) => {
+    if (event.target.classList.contains('tab_content_item')){
+        tabItems.forEach((tabItem, tabIndex) =>{
+            if (event.target.classList.contains('tab_content_item')){
+                tabItems.forEach((tabItem, tabIndex)=>{
+                    if (event.target ===tabItem) {
+                        hideTabContent()
+                        showTabContent(tabIndex)
+                    }
+                })
+            }
+        })
+    }
+}
+
+autoSlider()
